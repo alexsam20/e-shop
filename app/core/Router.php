82b4c5d0e6d18cf841fb1c3b732e549784bc275f
Alexandr SaM <alexsam2070@gmail.com>
2023-07-04 15:39:48 -0400

    Modified class Router add method matchRoute($url)

--- app/core/Router.php
+++ app/core/Router.php
@@ -24,6 +24,47 @@ class Router
 
     public static function dispatch($url)
     {
+        if (self::matchRoute($url)) {
+            echo 'OK';
+        } else {
+            echo 'NO';
+        }
+    }
 
+    public static function matchRoute($url): bool
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#i", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+                //print_pre($route);
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                //print_pre($route);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected static function upperCamelCase($name): string
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+    }
+
+    protected static function lowerCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
     }
 }
